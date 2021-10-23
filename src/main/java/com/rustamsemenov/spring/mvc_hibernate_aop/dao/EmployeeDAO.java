274d91463813bdbf5bbb91bd0c95e3f3358b6bfa
Rustam Semenov <88612028+SemenovRustam@users.noreply.github.com>
2021-10-23 16:23:30 +0500

    Add files via upload

---
+++ src/main/java/com/rustamsemenov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.rustamsemenov.spring.mvc_hibernate_aop.dao;
+
+import com.rustamsemenov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployee();
+
+   public    void saveEmployee(Employee employee);
+
+   public Employee getEmployee(int id);
+
+   public void deleteEmployee(int id);
+}
